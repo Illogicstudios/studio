e85e761952be7588f4145a4db7ed45f5c89ee521
mariusjenin <marius2401@laposte.net>
2023-02-28 12:46:48 +0100

    studio path fix

--- studio_loader.py
+++ studio_loader.py
@@ -3,7 +3,7 @@ from pymel.core import *
 
 
 def run():
-    studio_filepath = os.path.dirname(__file__) + "/assets/studio.mb"
+    studio_filepath = (os.path.dirname(__file__) + "/assets/studio.mb").replace("\\","/")
     # Find existing and remove reference if found
     for ref in listReferences():
         if ref == studio_filepath:
