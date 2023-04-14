54aa3fbf54b9844d94f3c2d3ca849399b61676af
mariusjenin <marius2401@laposte.net>
2023-04-14 16:34:56 +0200

    change import

--- studio_loader.py
+++ studio_loader.py
@@ -1,13 +1,13 @@
 import os
-from pymel.core import *
+import pymel.core as pm
 
 
 def run():
     studio_filepath = (os.path.dirname(__file__) + "/assets/studio.mb").replace("\\","/")
     # Find existing and remove reference if found
-    for ref in listReferences():
+    for ref in pm.listReferences():
         if ref == studio_filepath:
             ref.remove()
             return
     # If studio not existing create reference
-    createReference(studio_filepath)
+    pm.createReference(studio_filepath)
